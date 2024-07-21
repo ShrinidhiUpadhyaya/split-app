66a888f02936d79fe42fcfe0a20263be794f8b77
Shrinidhi Upadhyaya <shrinidhiupadhyaya1195@gmail.com>
2024-07-21 16:57:01 +0200

    fix: replace LogIn span with Link

--- split-app-frontend/components/Header.tsx
+++ split-app-frontend/components/Header.tsx
@@ -10,9 +10,13 @@ const Header = () => {
         <Image src={"logo.svg"} height={24} width={96} alt="logo" />
       </div>
       <div className="w-full flex gap-8 items-center justify-end flex-1">
-        <span className="primaryFontSize font-semibold cursor-pointer hover:underline">
+        <Link
+          href="/login"
+          className="primaryFontSize font-semibold cursor-pointer hover:underline"
+        >
           Log In
-        </span>
+        </Link>
+
         <Link href="" className={buttonVariants({ variant: "default" })}>
           Sign Up
         </Link>
