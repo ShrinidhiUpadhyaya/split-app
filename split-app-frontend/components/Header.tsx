1a15a3185721298212d59a6b4924a10675555075
Shrinidhi Upadhyaya <shrinidhiupadhyaya1195@gmail.com>
2024-07-21 10:56:50 +0200

    feat: header

---
+++ split-app-frontend/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { buttonVariants } from "./ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="w-full flex justify-between px-16 py-4">
+      <div className="flex-1">
+        <Image src={"logo.svg"} height={24} width={96} alt="logo" />
+      </div>
+      <div className="w-full flex gap-8 items-center justify-end flex-1">
+        <span className="primaryFontSize font-semibold cursor-pointer hover:underline">
+          Log In
+        </span>
+        <Link href="" className={buttonVariants({ variant: "default" })}>
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
