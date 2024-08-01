55245912fb4cca12d0e2a81a92dee4be6ff4f171
Shrinidhi Upadhyaya <shrinidhiupadhyaya1195@gmail.com>
2024-08-01 13:42:42 +0200

    fix(split-app-frontend): fixed header bar visibility in user and login page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,10 +1,23 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/navigation";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { LayoutDashboard, Handshake, Group } from "lucide-react";
 import { cn } from "@/lib/utils";
 import Dashboard from "@/app/user/Dashboard";
 import Friends from "@/app/user/Friends";
 import Groups from "@/app/user/Groups";
+import { useAppStore } from "@/store/zustand";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { logOut } from "@/lib/authApi";
+import useShowToast from "@/components/DToast";
+import Image from "next/image";
+import Link from "next/link";
 
 interface SideNavNarProps {
   className?: string;
@@ -31,33 +44,78 @@ const sideNavBarOptions = [
 ];
 
 const User: React.FC<SideNavNarProps> = ({ className }) => {
+  const { user, clearAll } = useAppStore();
+  const router = useRouter();
+  const { showSuccessToast } = useShowToast();
+
+  const onLogOut = async () => {
+    try {
+      await logOut();
+      clearAll();
+      router.push("/");
+      showSuccessToast("You have successfully logged out.");
+    } catch (error) {
+      console.log("Logout error", error);
+    }
+  };
+
   return (
-    <Tabs
-      defaultValue="dashboard"
-      className={cn("flex-1 w-full md:flex md:gap-12", className)}
-    >
-      <TabsList className="flex md:flex-col items-start gap-8 h-full md:w-[25%] w-full">
-        {sideNavBarOptions.map((option) => (
-          <TabsTrigger
-            key={option.value}
-            value={option.value}
-            className="max-w-[360px] w-full font-medium gap-4 flex justify-center md:justify-start h-12 rounded-md px-8 py-4"
-          >
-            <span>{option.icon}</span>
-            <span className="hidden sm:flex">{option.label}</span>
-          </TabsTrigger>
-        ))}
-      </TabsList>
-      <TabsContent value="dashboard">
-        <Dashboard />
-      </TabsContent>
-      <TabsContent value="groups">
-        <Groups />
-      </TabsContent>
-      <TabsContent value="friends" className="w-full">
-        <Friends />
-      </TabsContent>
-    </Tabs>
+    <>
+      <div className="w-full flex justify-between items-center gap-4 h-16 px-16">
+        <Link href="/">
+          <Image src={"logo.svg"} height={24} width={96} alt="Split logo" />
+        </Link>
+
+        <div className="flex-1 flex items-center justify-end">
+          <p>{user?.name ?? user?.email}</p>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Image
+                src="/guy1Profile.png"
+                height={24}
+                width={24}
+                alt="Profile Photo"
+              />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="shadow-lg w-40" align="end">
+              <DropdownMenuItem className="cursor-pointer">
+                Your Account
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer" onClick={onLogOut}>
+                Sign Out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+      <Tabs
+        defaultValue="dashboard"
+        className={cn("flex-1 w-full md:flex md:gap-12", className)}
+      >
+        <TabsList className="flex md:flex-col items-start gap-8 h-full md:w-[25%] w-full">
+          {sideNavBarOptions.map((option) => (
+            <TabsTrigger
+              key={option.value}
+              value={option.value}
+              className="max-w-[360px] w-full font-medium gap-4 flex justify-center md:justify-start h-12 rounded-md px-8 py-4"
+            >
+              <span>{option.icon}</span>
+              <span className="hidden sm:flex">{option.label}</span>
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        <TabsContent value="dashboard">
+          <Dashboard />
+        </TabsContent>
+        <TabsContent value="groups">
+          <Groups />
+        </TabsContent>
+        <TabsContent value="friends" className="w-full">
+          <Friends />
+        </TabsContent>
+      </Tabs>
+    </>
   );
 };
 
--- components/screenComponents/Header.tsx
+++ components/screenComponents/Header.tsx
@@ -3,44 +3,21 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { usePathname } from "next/navigation";
-import { useAppStore } from "@/store/zustand";
 import { cn } from "@/lib/utils";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import DPrimaryButtonLink from "@/components/DPrimaryButtonLink";
-import useShowToast from "@/components/DToast";
-import { logOut } from "@/lib/authApi";
 
-const authRoutes = ["/login", "/signup"];
+import DPrimaryButtonLink from "@/components/DPrimaryButtonLink";
+
+const headerRoutes = ["/"];
 
 const Header = () => {
   const pathname = usePathname();
-  const showHeader = !authRoutes.includes(pathname);
-  const { user, clearAll } = useAppStore();
-  const router = useRouter();
-  const { showSuccessToast } = useShowToast();
+  const showHeader = headerRoutes.includes(pathname);
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
-  const onLogOut = async () => {
-    try {
-      await logOut();
-      clearAll();
-      router.push("/");
-      showSuccessToast("You have successfully logged out.");
-    } catch (error) {
-      console.log("Logout error", error);
-    }
-  };
 
   return (
     <div
@@ -56,50 +33,20 @@ const Header = () => {
       </div>
 
       {isMounted && (
-        <>
-          {user ? (
-            <div className="flex items-center gap-4">
-              <p>{user.name ?? user.email}</p>
+        <div className="w-full flex gap-8 items-center justify-end flex-1">
+          <Link
+            href="/login"
+            className="primaryFontSize font-semibold cursor-pointer hover:underline"
+          >
+            Log In
+          </Link>
 
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <Image
-                    src="/guy1Profile.png"
-                    height={24}
-                    width={24}
-                    alt="Profile Photo"
-                  />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="shadow-lg w-40" align="end">
-                  <DropdownMenuItem className="cursor-pointer">
-                    Your Account
-                  </DropdownMenuItem>
-                  <DropdownMenuItem
-                    className="cursor-pointer"
-                    onClick={onLogOut}
-                  >
-                    Sign Out
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          ) : (
-            <div className="w-full flex gap-8 items-center justify-end flex-1">
-              <Link
-                href="/login"
-                className="primaryFontSize font-semibold cursor-pointer hover:underline"
-              >
-                Log In
-              </Link>
-
-              <DPrimaryButtonLink
-                href="/signup"
-                label="Sign Up"
-                className="w-min"
-              />
-            </div>
-          )}
-        </>
+          <DPrimaryButtonLink
+            href="/signup"
+            label="Sign Up"
+            className="w-min"
+          />
+        </div>
       )}
     </div>
   );
