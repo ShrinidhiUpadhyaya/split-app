860eb0195be4a262a092e6d43fafe7d7b2104072
Shrinidhi Upadhyaya <shrinidhiupadhyaya1195@gmail.com>
2024-12-07 20:22:00 +0100

    fix: path aliases in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname);
+    config.resolve.alias = {
+      "@": path.resolve(__dirname),
+      "@/components": path.resolve(__dirname, "components"),
+      "@/hooks": path.resolve(__dirname, "hooks"),
+    };
+
     return config;
   },
 };
